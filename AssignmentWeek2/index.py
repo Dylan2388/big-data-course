7c83dbb6e3e0fc96711ddb01f35a700bde0a60ed
Dylan N Pham <dylanpn@oxtros.dev>
2021-11-30 20:57:39 +0100

    update

--- AssignmentWeek2/index.py
+++ AssignmentWeek2/index.py
@@ -1,8 +1,16 @@
+"""
+Pham Nguyen Hoang Dung - s2845016
+Silvi - s2800209
+"""
+
 from pyspark import SparkContext
+import regex as re
 
 sc = SparkContext(appName="IINDEX")
-sc.setLogLevel("ERROR")
+# sc.setLogLevel("ERROR")
 
+def remove_punctuation(text):
+    return re.sub(ur"\p{P}+", "", text)
 
 def separate((path, contents)):
     return [(content, path) for content in contents]
@@ -18,10 +26,17 @@ def extend(a, b):
     a.extend(b)
     return a
 
+# read the document
 rdd = sc.wholeTextFiles("/data/doina/Gutenberg-EBooks")
-rdd2 = rdd.map(lambda (path, contents): (path, contents.lower().split()))
+# split the contents of file into words (after convert to lower case and remove punctuation)
+rdd2 = rdd.map(lambda (path, contents): (path, remove_punctuation(contents).lower().split()))
+# seperate. i.e: {(key, [value1, value2])} --> {(value1, key), (value2, key)}
 rdd3 = rdd2.flatMap(separate)
+# combine records with same key. i.e: {(key, value1), (key, value2)} --> {(key, [value1, value2])}
 rdd4 = rdd3.combineByKey(to_list, append, extend).map(lambda (key, value): (key, set(value)))
+# filter records with lengths of value at least 13
 rdd5 = rdd4.filter(lambda (key, value): len(value) >= 13)
+# sort rdd by key, then return list of key
 rdd6 = rdd5.sortByKey().keys()
-rdd6.take(100)
+# merge and print
+' '.join(rdd6.take(1000))
