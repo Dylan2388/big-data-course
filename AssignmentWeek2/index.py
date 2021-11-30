bfeaa46cc857770d9def6adfc7019b8556416f70
Dylan N Pham <dylanpn@oxtros.dev>
2021-11-30 20:00:28 +0100

    update assignment2 iindex

---
+++ AssignmentWeek2/index.py
@@ -0,0 +1,31 @@
+from pyspark import SparkContext
+from time import time
+
+# sc = SparkContext(appName="IINDEX")
+# sc.setLogLevel("ERROR")
+
+
+def separate((path, contents)):
+    return [(content, path) for content in contents]
+
+def to_list(a):
+    return [a]
+
+def append(a, b):
+    a.append(b)
+    return a
+
+def extend(a, b):
+    a.extend(b)
+    return a
+
+start_time = time()
+rdd = sc.wholeTextFiles("/data/doina/Gutenberg-EBooks")
+rdd2 = rdd.map(lambda (path, contents): (path, contents.lower().split()))
+rdd3 = rdd2.flatMap(separate)
+rdd4 = rdd3.combineByKey(to_list, append, extend).map(lambda (key, value): (key, set(value)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+# rdd5 = rdd4.filter(lambda (key, value): len(value) >= 13)
+# rdd6 = rdd5.sortByKey().keys()
+# rdd6.take(100)
