4e37f7cd36bdaac78d411da8e793addaa7d36351
Dylan N Pham <dylanpn@oxtros.dev>
2021-11-30 21:00:53 +0100

    update

--- AssignmentWeek2/IINDEX-s2845016-s2800209-RVSSTR.py
+++ AssignmentWeek2/IINDEX-s2845016-s2800209-RVSSTR.py
@@ -1,13 +1,18 @@
 """
 Pham Nguyen Hoang Dung - s2845016
 Silvi - s2800209
+
+Run time:
+real	0m9.883s
+user	0m14.648s
+sys	    0m1.354s
 """
 
 from pyspark import SparkContext
 import regex as re
 
 sc = SparkContext(appName="IINDEX")
-# sc.setLogLevel("ERROR")
+sc.setLogLevel("ERROR")
 
 def remove_punctuation(text):
     return re.sub(ur"\p{P}+", "", text)
@@ -31,7 +36,7 @@ rdd = sc.wholeTextFiles("/data/doina/Gutenberg-EBooks")
 # split the contents of file into words (after convert to lower case and remove punctuation)
 rdd2 = rdd.map(lambda (path, contents): (path, remove_punctuation(contents).lower().split()))
 # seperate. i.e: {(key, [value1, value2])} --> {(value1, key), (value2, key)}
-rdd3 = rdd2.flatMap(separate)
+rdd3 = rdd2.flatMap(separate())
 # combine records with same key. i.e: {(key, value1), (key, value2)} --> {(key, [value1, value2])}
 rdd4 = rdd3.combineByKey(to_list, append, extend).map(lambda (key, value): (key, set(value)))
 # filter records with lengths of value at least 13
