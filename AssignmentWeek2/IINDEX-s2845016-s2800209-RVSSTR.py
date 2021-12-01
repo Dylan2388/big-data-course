9dc734126f28cc0da17d96daa0906cf5cf814ec2
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-01 12:24:44 +0100

    update

--- AssignmentWeek2/IINDEX-s2845016-s2800209-RVSSTR.py
+++ AssignmentWeek2/IINDEX-s2845016-s2800209-RVSSTR.py
@@ -39,9 +39,9 @@ rdd2 = rdd.map(lambda (path, contents): (path, remove_punctuation(contents).lowe
 # seperate. i.e: {(key, [value1, value2])} --> {(value1, key), (value2, key)}
 rdd3 = rdd2.flatMap(separate)
 # combine records with same key. i.e: {(key, value1), (key, value2)} --> {(key, [value1, value2])}
-rdd4 = rdd3.combineByKey(to_list, append, extend).map(lambda (key, value): (key, set(value)))
+final_result = rdd3.combineByKey(to_list, append, extend).map(lambda (key, value): (key, list(set(value))))
 # filter records with lengths of value at least 13
-rdd5 = rdd4.filter(lambda (key, value): len(value) >= 13)
+rdd5 = final_result.filter(lambda (key, value): len(value) >= 13)
 # sort rdd by key, then return list of key
 rdd6 = rdd5.sortByKey().keys()
 # merge and print
