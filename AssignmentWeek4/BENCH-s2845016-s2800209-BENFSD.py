7c093838c398e7f884541a45bc083b60ad280b88
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-14 19:29:56 +0100

    update

--- AssignmentWeek4/BENCH-s2845016-s2800209-BENFSD.py
+++ AssignmentWeek4/BENCH-s2845016-s2800209-BENFSD.py
@@ -33,27 +33,27 @@ from pyspark.sql.functions import col
 
 spark = SparkSession.builder.getOrCreate()
 
-### Fix number of executors: 10, executor memory: 3G. Overall, the total capacity of executors is 30GB.
-### A day of tweets is ~1.44 GB compressed (in 1440 files). We assume after extracting, the amount of data is tripled (assume 5GB)
-### 1-day data: 4.5GB
+### Fix number of executors: 10, executor memory: 2G. Overall, the total capacity of executors is 20GB.
+### A day of tweets is ~1.44 GB compressed (in 1440 files). We assume after extracting, the amount of data is double or tripled (assume 3-4.5GB)
+### 1-day data: 3-4.5GB
 PATH_1_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-0]/*/*.json.bz2"
-### 2-day data: 9GB
+### 2-day data: 6-9GB
 PATH_2_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-1]/*/*.json.bz2"
-### 3-day data: 13.5GB
+### 3-day data: 9-13.5GB
 PATH_3_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-2]/*/*.json.bz2"
-### 4-day data: 18GB
+### 4-day data: 12-18GB
 PATH_4_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-3]/*/*.json.bz2"
-### 5-day data: 22.5GB
+### 5-day data: 15-22.5GB (Assumption: The plot should be bumped at this point or onward.)
 PATH_5_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-4]/*/*.json.bz2"
-### 6-day data: 27GB 
+### 6-day data: 18-27GB 
 PATH_6_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-5]/*/*.json.bz2"
-### 7-day data: 31.5GB (Assumption: The plot should be bumped at this point.)
+### 7-day data: 21-31.5GB 
 PATH_7_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-6]/*/*.json.bz2"
-### 8-day data: 36GB
+### 8-day data: 24-36GB
 PATH_8_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-7]/*/*.json.bz2"
-### 9-day data: 40.5GB
+### 9-day data: 27-40.5GB
 PATH_9_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-8]/*/*.json.bz2"
-### 10-day data: 45GB
+### 10-day data: 30-45GB
 PATH_10_DAY = "/data/doina/Twitter-Archive.org/2017-01/1[0-9]/*/*.json.bz2"
 
 # a regexp, case-insensitive matching; this was a topic of interest on those dates (Jan 2017)
