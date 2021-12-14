d7225ed617ed276b474586b770cb5f6e337262b5
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-14 19:34:34 +0100

    update

--- AssignmentWeek4/BENCH-s2845016-s2800209-BENFSD.py
+++ AssignmentWeek4/BENCH-s2845016-s2800209-BENFSD.py
@@ -62,7 +62,7 @@ KEYWORDS = "(inauguration)|(whitehouse)|(washington)|(president)|(obama)|(trump)
 # always creates a new folder to write in (clean up your old folders though)
 now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-tweets = spark.read.json(PATH_1_DAY) \
+tweets = spark.read.json(PATH_2_DAY) \
     .filter(col("text").isNotNull()) \
     .select(col("text")) \
     .filter(col("text").rlike(KEYWORDS).alias("text")) \
