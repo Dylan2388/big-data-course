e216764c70cc96c4a45346ed599b1c2a4f249816
Dylan N Pham <dylanpn@oxtros.dev>
2021-12-16 17:32:26 +0100

    update

--- AssignmentWeek4/BENCH-plot.py
+++ AssignmentWeek4/BENCH-plot.py
@@ -20,6 +20,7 @@ plt.plot("Days", "Time", data=d)
 plt.xlim(-.2, 10.2)
 plt.ylim(-.4, 14.4)
 plt.xticks(days)
+plt.yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 plt.xlabel("Amount of data by Day (each day ~ 1.6GB compressed)")
 plt.ylabel("Process Time by Minute")
 plt.title("Spark Processed Time by amount of data \n (Number of Executor = 10, Memory each executor = 2GB)",
