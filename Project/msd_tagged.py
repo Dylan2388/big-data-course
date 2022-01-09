c8d19b4330a1192037841978048bff3803dac5f8
raefkazi <96086431+raefkazi@users.noreply.github.com>
2022-01-09 14:59:58 +0100

    Update msd_tagged.py

--- Project/msd_tagged.py
+++ Project/msd_tagged.py
@@ -9,9 +9,10 @@ df1 = spark.read.option("header",True).option("quote", "\"").option("escape", "\
 
 #Read all lastfm data
 lastfm = spark.read.json("/user/s2733226/project/lastfm_preprocessed/*.json")
+lastfm_1 = lastfm.select(col('track_id'),col('similars'),col('tags'))
 
 #Join tables
-df_joined = df1.join(lastfm, 'track_id')
+df_joined = df1.join(lastfm_1, 'track_id')
 
 #Remove duplicates, select required columns
 df_joined = df_joined.select(col('track_id'),col('artist_mbid'),col('artist_name'),col('artist_playmeid'),col('artist_terms'),col('artist_terms_freq'),col('artist_terms_weight'),col('audio_md5'),col('bars_confidence'),col('bars_start'),col('beats_confidence'),col('beats_start'),col('danceability'),col('duration'),col('end_of_fade_in'),col('energy'),col('key'),col('key_confidence'),col('loudness'),col('mode'),col('mode_confidence'),col('release'),col('release_7digitalid'),col('sections_confidence'),col('sections_start'),col('segments_confidence'),col('segments_loudness_max'),col('segments_loudness_max_time'),col('segments_loudness_start'),col('segments_pitches'),col('segments_start'),col('segments_timbre'),col('similar_artists'),col('song_hotttnesss'),col('song_id'),col('start_of_fade_out'),col('tatums_confidence'),col('tatums_start'),col('tempo'),col('time_signature'),col('time_signature_confidence'),col('title'),col('track_7digitalid'),col('year'),col('similars'),col('tags')).distinct().coalesce(50)
