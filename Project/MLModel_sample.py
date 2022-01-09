a6b11ecaa2aa1fc72a0a308235e3b86caa977806
Dylan N Pham <dylanpn@oxtros.dev>
2022-01-09 12:10:02 +0100

    update

--- Project/MLModel_sample.py
+++ Project/MLModel_sample.py
@@ -1,9 +1,18 @@
+"""[summary]
+pyspark --conf "spark.pyspark.python=/usr/bin/python3.6" --conf "spark.pyspark.driver.python=/usr/bin/python3.6"
+"""
+
+
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.classification import MultilayerPerceptronClassifier
 
 spark = SparkSession.builder.getOrCreate()
 
-df = (spark.read
-          .format("csv")
-          .option('header', 'true')
-          .load("train.csv"))
+df = spark.createDataFrame([
+    (0.0, Vectors.dense([0.0, 0.0])),
+    (1.0, Vectors.dense([0.0, 1.0])),
+    (1.0, Vectors.dense([1.0, 0.0])),
+    (0.0, Vectors.dense([1.0, 1.0]))], ["label", "features"])
+mlp = MultilayerPerceptronClassifier(layers=[2, 2, 2], seed=123)
