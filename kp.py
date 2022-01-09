cf6ca614460c37c613a632b435917b3e19a83eb2
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-01-09 15:52:32 +0100

    deleting scratch file

--- kp.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-genres = pd.read_csv('http://www.millionsongdataset.com/sites/default/files/lastfm/lastfm_unique_tags.txt', sep=" ", header=None)
-genres.columns = ["genre", "freq"]
-print(genres)
