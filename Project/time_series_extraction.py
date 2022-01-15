640c2a67b7f7525ad04a628ad43e6d9130056bc4
Dylan N Pham <dylanpn@oxtros.dev>
2022-01-15 18:56:03 +0100

    update

--- Project/time_series_extraction.py
+++ Project/time_series_extraction.py
@@ -5,12 +5,10 @@ from pyspark.sql.types import *
 
 spark = SparkSession.builder.getOrCreate()
 
-df_section_start = spark.read.csv("/user/s2733226/project/column_data/sections_start/part-0*.csv")
-df_section_start = df_section_start.selectExpr("_c0 as name", "_c1 as value")
-df_section_start = df_section_start.distinct()
+df = spark.read.csv("/user/s2733226/project/column_data/sections_start/part-0*.csv")
+df = df.selectExpr("_c0 as name", "_c1 as value")
 
 
 from pyspark.sql.types import ArrayType, IntegerType
-
-df_section_start = df_section_start.withColumn("value", df_section_start["value"].substr(1,len(df_section_start["value"])))
+df_section_start = df.withColumn("value", F.expr("substring(value, 2, length(value) - 2)"))
 df_section_start_array = df_section_start.withColumn("value",F.split(F.col("value"), ",\s*").cast(ArrayType(IntegerType())).alias("value"))
